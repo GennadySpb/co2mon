ab8632efb25355ae213046f77fd34c97661b6ab6
Gennady Lipenkov <lipenkov@gmail.com>
2023-03-02 06:54:22 +0300

    Draft 2

--- cmd/ycpusher/ycpusher.go
+++ cmd/ycpusher/ycpusher.go
@@ -7,6 +7,7 @@ import (
 	"flag"
 	"fmt"
 	ycsdk "github.com/yandex-cloud/go-sdk"
+	"io"
 	"log"
 	"net/http"
 	"time"
@@ -19,10 +20,21 @@ var folderID string
 
 func main() {
 
-	flag.StringVar(&ServiceAccountKeyFile, "sa-key-file", "sa.json", "Service account key file")
+	flag.StringVar(&ServiceAccountKeyFile, "sa-key-file", "", "Service account key file")
 	flag.StringVar(&Token, "token", "", "Yandex.Cloud token")
 	flag.StringVar(&folderID, "folder-id", "", "Yandex.Cloud folder ID")
 	flag.Parse()
+
+	// validate
+	if folderID == "" {
+		log.Fatal("validate error: folder-id is required")
+	}
+
+	if ServiceAccountKeyFile != "" && Token != "" {
+		log.Fatal("validate error: only one of 'token' or 'sa-key-file' should be specified")
+	}
+
+	// init
 	ctx := context.Background()
 
 	credentials, err := Credentials()
@@ -39,24 +51,27 @@ func main() {
 	}
 
 	tokenMiddleware := ycsdk.NewIAMTokenMiddleware(sdk, time.Now)
+	iamToken, err := tokenMiddleware.GetIAMToken(ctx, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sendMetrics(ctx, iamToken)
 
 	ticker := time.NewTicker(15 * time.Second)
 	quit := make(chan struct{})
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				iamToken, err := tokenMiddleware.GetIAMToken(ctx, false)
-				if err != nil {
-					log.Fatal(err)
-				}
-				sendMetrics(ctx, iamToken)
-			case <-quit:
-				ticker.Stop()
-				return
+	for {
+		select {
+		case <-ticker.C:
+			iamToken, err := tokenMiddleware.GetIAMToken(ctx, false)
+			if err != nil {
+				log.Fatal(err)
 			}
+			sendMetrics(ctx, iamToken)
+		case <-quit:
+			ticker.Stop()
+			return
 		}
-	}()
+	}
 }
 
 var endpoint = "monitoring.api.cloud.yandex.net"
@@ -70,7 +85,7 @@ func sendMetrics(ctx context.Context, iamToken string) {
 	data := MonitoringStruct{}
 	data1 := Metric{
 		Name:   "Temp",
-		Labels: nil,
+		Labels: map[string]string{},
 		Value:  10,
 	}
 	data.Metrics = append(data.Metrics, data1)
@@ -86,8 +101,10 @@ func sendMetrics(ctx context.Context, iamToken string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	req.URL.Query().Add("folderId", folderID)
-	req.URL.Query().Add("service", "custom")
+	q := req.URL.Query()
+	q.Add("folderId", folderID)
+	q.Add("service", "custom")
+	req.URL.RawQuery = q.Encode()
 
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", iamToken))
@@ -98,5 +115,14 @@ func sendMetrics(ctx context.Context, iamToken string) {
 		log.Fatal(err)
 	}
 	defer response.Body.Close()
-
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if response.StatusCode != http.StatusOK {
+		log.Printf("response Status: %s", response.Status)
+		log.Printf("response Body: %s", body)
+	}
+	//log.Printf("response Status: %s", response.Status)
+	//log.Printf("response Body: %s", body)
 }
