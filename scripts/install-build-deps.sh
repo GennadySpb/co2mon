b3b0f2d6745791ab30e5f991e00dae01938468f7
Gennady Lipenkov <lipenkov@gmail.com>
2023-07-25 11:16:37 +0300

    Add arch-specific libs to docker container

--- scripts/install-build-deps.sh
+++ scripts/install-build-deps.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-apt-get update -qq && apt-get install -y libudev-dev libhidapi-dev
+dpkg --add-architecture amd64 armhf
+apt-get update -qq && apt-get install -y libudev-dev libudev-dev:amd64 libudev-dev:armhf
 dpkg -S libudev.h
